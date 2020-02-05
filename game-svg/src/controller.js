8812b99b203db8f4a845120dbd70b9a741f66712
TykhonchukIvan <tykhonchukivan@gmail.com>
2020-02-05 23:34:46 +0200

    controller

---
+++ game-svg/src/controller.js
@@ -0,0 +1,11 @@
+function BallsSVG(svgObj){
+    this.svg = document.getElementById(svgObj);
+    let obj = this;
+    this.svg.addEventListener('click', event => {
+        obj.createBall(event);
+        obj.createBall(event);
+        obj.createBall(event);
+    });
+    this.items = this.svg.getElementsByTagName("circle");
+
+}
